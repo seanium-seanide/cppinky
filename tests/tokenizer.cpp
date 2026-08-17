c5ddef1b081baaa1a6e4064592d6dc44a8f3221a
Sean Macfoy <sean_macfoy@outlook.com>
2026-08-17 01:25:45 +0100

    Split tests

--- tests/tokenizer.cpp
+++ tests/tokenizer.cpp
@@ -1,40 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 
-#include <sstream>
 #include <vector>
 #include <ranges>
 #include <utilities.hpp>
 #include <Tokenizer.hpp>
 
 using namespace std::literals;
-
-
-TEST_CASE("Reading files", "[utilities]")
-{
-  SECTION("When readStream is called on an input stream, it returns the characters found on the stream.")
-  {
-    std::istringstream iss;
-
-    auto expectedResult = GENERATE("", "Hello, world!");
-    iss.str(expectedResult);
-    auto result = utilities::readStream(iss);
-
-    REQUIRE(result == expectedResult);
-  }
-
-  SECTION("When readStream is called on an input stream, subsequent reads return the empty string.")
-  {
-    std::istringstream iss;
-
-    auto expectedResult = GENERATE("", "Hello, world!");
-    iss.str(expectedResult);
-    auto result = utilities::readStream(iss);
-    result = utilities::readStream(iss);
-
-    REQUIRE(result == "");
-  }
-}
 
 
 TEST_CASE("Tokenizing scripts", "[cppinky]")
---
+++ tests/utilities.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <sstream>
+#include <utilities.hpp>
+
+using namespace std::literals;
+
+
+TEST_CASE("Reading files", "[utilities]")
+{
+  SECTION("When readStream is called on an input stream, it returns the characters found on the stream.")
+  {
+    std::istringstream iss;
+
+    auto expectedResult = GENERATE("", "Hello, world!");
+    iss.str(expectedResult);
+    auto result = utilities::readStream(iss);
+
+    REQUIRE(result == expectedResult);
+  }
+
+  SECTION("When readStream is called on an input stream, subsequent reads return the empty string.")
+  {
+    std::istringstream iss;
+
+    auto expectedResult = GENERATE("", "Hello, world!");
+    iss.str(expectedResult);
+    auto result = utilities::readStream(iss);
+    result = utilities::readStream(iss);
+
+    REQUIRE(result == "");
+  }
+}
